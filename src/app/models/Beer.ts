9410faa02a058dd82518d919d41509bd01cb06ff
Sarunas Dovydaitis <82829974+SarunasDov@users.noreply.github.com>
2021-10-28 20:11:08 +0300

    brewerie pradzia

---
+++ src/app/models/Beer.ts
@@ -0,0 +1,9 @@
+export class Beer {
+    public id: number = 0;
+    public name: string = '';
+    public description: string = '';
+    public image_url: string = '';
+    public food_pairing: string [] = [];
+    public abv: number = 0;
+    public ph: number = 0;
+}
